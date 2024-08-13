e4976890cdf4da813522092bfd392fb1f8847467
Facundo Grondona <facundo.grondona.02@gmail.com>
2024-08-13 10:05:16 -0300

    Manejando el estado del delete

--- src/main/java/com/api/facu/api_rest_facu/controllers/ClienteControler.java
+++ src/main/java/com/api/facu/api_rest_facu/controllers/ClienteControler.java
@@ -4,8 +4,13 @@ import com.api.facu.api_rest_facu.Models.entity.Cliente;
 import com.api.facu.api_rest_facu.servicio.IClienteServicio;
 import com.api.facu.api_rest_facu.servicio.implementaciones.ClienteIMPL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,10 +34,24 @@ public class ClienteControler {
     }
 
     @DeleteMapping("cliente/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Integer id) {
-        Cliente nuevoCliente = clienteIMPL.finById(id);
-        clienteIMPL.delete(nuevoCliente);
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+
+        Map<String, Object> mapErrors = new HashMap<>();
+
+        try{
+            Cliente nuevoCliente = clienteIMPL.finById(id);
+            clienteIMPL.delete(nuevoCliente);
+
+            return new ResponseEntity<>(nuevoCliente, HttpStatus.NO_CONTENT);
+
+        }catch (DataAccessException ex){
+
+             mapErrors.put("mensaje", ex.getMessage());
+             mapErrors.put("cliente",null);
+            return new ResponseEntity<>(mapErrors, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
     }
 
     @GetMapping("cliente/{id}")
